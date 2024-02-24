85c96bca433b373f3499995113dc7cce1a68e1d0
sumanmedda <sumanmedda33@gmail.com>
2024-02-24 20:14:27 +0530

    final-update1

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[STATIC_DIR,]

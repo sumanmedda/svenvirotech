535517cd6abefbe1589073fee829c8b1e943b17f
sumanmedda <sumanmedda33@gmail.com>
2024-02-26 10:30:02 +0530

    final-update2

--- ecom/models.py
+++ ecom/models.py
@@ -2,7 +2,8 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name=models.CharField(max_length=40)
-    product_image= models.ImageField(upload_to='product_image/',null=True,blank=True)
+    # product_image= models.ImageField(upload_to='product_image/',null=True,blank=True)
+    product_image= models.ImageField(upload_to='',null=True,blank=True)
     price = models.PositiveIntegerField()
     description=models.CharField(max_length=255)
     def __str__(self):
